cdf2c1d28b837179f46435fadd28cbf598f0cc78
Aaron B Laptop <aaronber@pdx.edu>
2023-12-30 15:37:48 -0800

    4

--- VulkanTest/lve_pipeline.hpp
+++ VulkanTest/lve_pipeline.hpp
@@ -6,7 +6,20 @@
 
 namespace lve {
 
-	struct PipelineConfigInfo {};
+	struct PipelineConfigInfo {
+		VkViewport viewport;
+		VkRect2D scissor;
+		VkPipelineViewportStateCreateInfo viewportInfo;
+		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+		VkPipelineMultisampleStateCreateInfo multisampleInfo;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment;
+		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+		VkPipelineLayout pipelineLayout = nullptr;
+		VkRenderPass renderPass = nullptr;
+		uint32_t subpass = 0;
+	};
 
 	class LvePipeline {
 	public:
@@ -15,7 +28,7 @@ namespace lve {
 			const std::string& vertFilepath, 
 			const std::string& fragFilepath, 
 			const PipelineConfigInfo& configInfo);
-		~LvePipeline() {}
+		~LvePipeline();
 	
 		LvePipeline(const LvePipeline&) = delete;
 		void operator=(const LvePipeline&) = delete;
