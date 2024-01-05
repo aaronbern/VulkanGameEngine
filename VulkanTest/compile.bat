02dea53c77d7b642dd79cf426ec793d3f336a1ec
Aaron.Bernard <aaronber@pdx.edu>
2024-01-05 03:09:49 -0800

    merge conflicts and multi-device programming support variables added

--- VulkanTest/compile.bat
+++ VulkanTest/compile.bat
@@ -1,12 +1,12 @@
 @echo off
-"C:\VS Libraries\VSDK\Bin\glslc.exe" shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+"%VULKAN_SDK%\Bin\glslc.exe" shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
 if %ERRORLEVEL% EQU 0 (
     echo Vertex shader compiled successfully.
 ) else (
     echo Vertex shader compilation failed.
 )
 
-"C:\VS Libraries\VSDK\Bin\glslc.exe" shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+"%VULKAN_SDK%\Bin\glslc.exe" shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
 if %ERRORLEVEL% EQU 0 (
     echo Fragment shader compiled successfully.
 ) else (
